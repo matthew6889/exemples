36db7f2066f576990a65c2c91a04e84f6247ae88
Matvey <matveypashnev6889@gmail.com>
2023-07-02 14:57:02 +0400

    random

--- div/Program.cs
+++ div/Program.cs
@@ -1,6 +1,18 @@
-﻿int numberA = 10;
+﻿//Первый пример
+int numberA = 10;
 int numberB = 5;
 Console.WriteLine(numberA / numberB);
+Console.WriteLine("-----------");
+// Второй пример
 double numberC = 12;
 double numberD = 5;
 Console.WriteLine(numberC / numberD);
+Console.WriteLine("-----------");
+// Третий пример
+int numberE = new Random().Next(1,10);//1 2 3 4 5 ..... 9
+Console.WriteLine(numberE);
+int numberF = new Random().Next(1,10);
+Console.WriteLine(numberF);
+int result = numberE + numberF;
+Console.WriteLine(result);
+Console.WriteLine("-----------");
