9d71aba2f9d6ff6a1ad139a11163ea397d0ccf59
Matvey <matveypashnev6889@gmail.com>
2023-07-02 16:17:59 +0400

    home work

---
+++ dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int count = 0;
+int distance = 10000;
+int time = 0;
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance/(secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    if(friend == 2)
+    {
+        time = distance/(firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.Write(" раз");
+
