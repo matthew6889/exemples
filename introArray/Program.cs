e90d9728b9eeb6f58d37c6e41d967b49d834447e
Matvey <matveypashnev6889@gmail.com>
2023-07-05 21:10:58 +0400

    второе занятие

---
+++ introArray/Program.cs
@@ -0,0 +1,81 @@
+﻿// int Max(int arg1, int arg2, int arg3)
+// {
+//     int result = arg1;
+//     if(arg2 > result) result = arg2;
+//     if(arg3 > result) result = arg3;
+//     return result;
+// }
+// int[] array = {122,233,34,451,52,6,74,89,920};
+// array[0]=12;
+// Console.WriteLine(array[0]);
+// int result = Max
+//             (
+//               Max(array[0],array[1],array[2]),
+//               Max(array[3],array[4],array[5]),
+//               Max(array[6],array[7],array[8])
+//             );
+// Console.WriteLine(result);
+
+
+// int[] array = {1,22,3,4,5,64,7,80};
+// int n = array.Length;
+// int find = 80;
+// int index = 0;
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     index++;
+// }
+
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
